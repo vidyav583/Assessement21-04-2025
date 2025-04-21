47c3f37e72dda5deb17c88078fb8b881451dd39e
vidyav583 <vidyavshetty123@gmail.com>
2025-04-21 18:43:44 +0530

    Add files via upload

---
+++ FrontEnd(test)/assess1/function.js
@@ -0,0 +1,34 @@
+//problem1
+//functions
+
+function countVowels(str){
+let count=0;
+if( str ==='a'|| 'e' || "i"|| 'o' || 'u'){
+    count++;
+}
+return count;
+}
+console.log(countVowels("i"))
+console.log(count);
+
+
+//problem2
+flattenAndSort([[3, 2, 1], [4, 5, 2], [1, 6]])
+function sorted(flattenAndSort){
+    return split().sorted(" ").join()
+}
+
+
+//problem3
+
+const expenses = [
+    { category: "Food", amount: 120 },
+    { category: "Travel", amount: 300 },
+    { category: "Food", amount: 80 },
+    { category: "Bills", amount: 200 },
+    { category: "Travel", amount: 100 },
+    ];
+function getCategorySummary(expenses){
+    return expenses.Food 
+}
+console.log(expenses);
